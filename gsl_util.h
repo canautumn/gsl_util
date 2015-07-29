816ba24606dc3679b2057a9a2a8a878fb6702596
Can <kfjincan@gmail.com>
2015-07-29 16:13:21 -0400

    Add covariance matrix by matrix multiplication, and intrusive version.

--- gsl_util.h
+++ gsl_util.h
@@ -7,11 +7,13 @@
 #ifndef GSL_UTIL_H
 #define GSL_UTIL_H
 
+
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_linalg.h>
 #include <gsl/gsl_vector_double.h>
-
+#include <gsl/gsl_statistics_double.h>
+#include <gsl/gsl_matrix_double.h>
 
 //
 // gsl_util_pinv
@@ -45,9 +47,9 @@ void gsl_util_pinv(const gsl_matrix *A_c, gsl_matrix *pinv) {
 // mean_of
 // Calculate mean value after applying a lambda function. Useful for mean and variance.
 // The data must be represented by a vector. Data1 and 2 must have the same size.
-// If the sampled == 1, the mean is a sampled mean (the denominator is the number of samples minus 1).
+// If the unbiased == 1, the mean is the unbiased estimated sample mean (the denominator is the number of samples minus 1).
 //
-double mean_of(const gsl_vector * data1, const gsl_vector * data2, std::function<double(double, double)> pre_operation, int sampled = 0) {
+double mean_of(const gsl_vector * data1, const gsl_vector * data2, std::function<double(double, double)> pre_operation, int unbiased = 0) {
 
     if (data1->size != data2->size) {
         std::cerr << "Vector size error" << endl;
@@ -57,20 +59,22 @@ double mean_of(const gsl_vector * data1, const gsl_vector * data2, std::function
     for (size_t ob = 0; ob < data1->size; ++ob) {
         sum += pre_operation(gsl_vector_get(data1, ob), gsl_vector_get(data2, ob));
     }
-    sum /= data1->size - sampled;
+    sum /= data1->size - unbiased;
     return sum;
 }
 
 
 //
-// gsl_util_covariance_matrix
-// Calculate covariance matrix. The covariance matrix must be a square matrix with the
+// gsl_util_covariance_matrix_loop
+// Calculate covariance matrix using loops by definition of covariances.
+// CAUTION: It is much slower than the matrix multiplication version (without '_loop' in the function name).
+// The covariance matrix must be a square matrix with the
 // length equal to the second dimension of the matrix to be calculated.
+// The unbiased is 1 by default to calculate the unbiased estimated sample covariances
 //
-void gsl_util_covariance_matrix(const gsl_matrix * data, gsl_matrix * covariance) {
+void gsl_util_covariance_matrix_loop(const gsl_matrix * data, gsl_matrix * covariance, int unbiased = 1) {
 
     size_t dims = data->size2;
-    size_t obs = data->size1;
     if (covariance->size1 != covariance->size2 || covariance->size2 != dims) {
         std::cerr << "Dimension error" << endl;
         return;
@@ -93,13 +97,77 @@ void gsl_util_covariance_matrix(const gsl_matrix * data, gsl_matrix * covariance
             double means_dim2 = means[dim2];
             double cov = mean_of(&data1.vector, &data2.vector, [means_dim1, means_dim2] (double el1, double el2) {
                return (el1 - means_dim1) * (el2 - means_dim2);
-            }, 1);
+            }, unbiased);
             gsl_matrix_set(covariance, dim1, dim2, cov);
             gsl_matrix_set(covariance, dim2, dim1, cov);
         }
     }
 
     delete means;
+}
+
+//
+// gsl_util_covariance_matrix_intrusive
+// Calculate covariance matrix using matrix multiplication and intrusively changes input data to save space and time.
+// The input data will become the centered data (data matrix minus mean vector).
+// The unbiased is 1 by default to calculate the unbiased estimated sample covariances
+//
+void gsl_util_covariance_matrix_intrusive(gsl_matrix * data, gsl_matrix * covariance, int unbiased = 1) {
+
+    size_t dims = data->size2;
+    size_t obs = data->size1;
+    if (covariance->size1 != covariance->size2 || covariance->size2 != dims) {
+        std::cerr << "Dimension error" << endl;
+        return;
+    }
+
+    gsl_vector * means = gsl_vector_alloc(dims);
+    for (size_t c = 0; c < dims; ++c) {
+        gsl_vector_set(means, c, gsl_stats_mean(data->data + c, dims, obs));
+    }
+
+    for (size_t r = 0; r < obs; ++r) {
+        gsl_vector_view row = gsl_matrix_row(data, r);
+        gsl_vector_sub(&row.vector, means);
+    }
+
+    gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1.0, data, data, 0.0, covariance);
+    gsl_matrix_scale(covariance, 1.0 / (obs - unbiased));
+    gsl_vector_free(means);
+}
+
+//
+// gsl_util_covariance_matrix
+// Calculate covariance matrix using matrix multiplication.
+// The unbiased is 1 by default to calculate the unbiased estimated sample covariances
+//
+void gsl_util_covariance_matrix(const gsl_matrix * data, gsl_matrix * covariance, int unbiased = 1) {
+
+    size_t dims = data->size2;
+    size_t obs = data->size1;
+    if (covariance->size1 != covariance->size2 || covariance->size2 != dims) {
+        std::cerr << "Dimension error" << endl;
+        return;
+    }
+
+    gsl_vector * means = gsl_vector_alloc(dims);
+    for (size_t c = 0; c < dims; ++c) {
+        gsl_vector_set(means, c, gsl_stats_mean(data->data + c, dims, obs));
+    }
+    gsl_matrix * centered_data = gsl_matrix_alloc(obs, dims);
+
+    for (size_t r = 0; r < obs; ++r) {
+        for (size_t c = 0; c < dims; ++c) {
+            gsl_matrix_set(centered_data, r, c,
+                           gsl_matrix_get(data, r, c) - gsl_vector_get(means, c));
+        }
+    }
+
+    gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1.0, centered_data, centered_data, 0.0, covariance);
+    gsl_matrix_scale(covariance, 1.0 / (obs - unbiased));
+    gsl_vector_free(means);
+    gsl_matrix_free(centered_data);
+
 }
 
 
